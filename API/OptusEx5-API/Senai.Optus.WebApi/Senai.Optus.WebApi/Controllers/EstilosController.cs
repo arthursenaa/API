8da61a777fb877c8cd9961e7a3dee00372c73e6f
Arthur Senna de Andrade Souza <44761640804@info.local>
2019-08-26 17:28:07 -0300

    API

---
+++ API/OptusEx5-API/Senai.Optus.WebApi/Senai.Optus.WebApi/Controllers/EstilosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Optus.WebApi.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Senai.Optus.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+
+    public class EstilosController : Controller
+    {
+
+        EstilosRepository EstilosRepository = new EstilosRepository();
+
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(EstilosRepository.Listar());
+        }
+    }
+}
---
+++ API/OptusEx5-API/Senai.Optus.WebApi/Senai.Optus.WebApi/Repositories/EstilosRepository.cs
@@ -0,0 +1,19 @@
+﻿using Senai.Optus.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Optus.WebApi.Repositories
+{
+    public class EstilosRepository
+    {
+        public List<Estilos> Listar()
+        {
+            using (OptusContext ctx = new OptusContext())
+            {
+                return ctx.Estilos.ToList();
+            }
+        }
+    }
+}
